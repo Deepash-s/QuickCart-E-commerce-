9a4c10aab2f979214645cd2c36696163803667b2
Deepash S <deepashs18@gmail.com>
2024-09-14 19:16:25 +0530

    Add files via upload

---
+++ collections.js
@@ -0,0 +1,19 @@
+var collectionsconatiner =  document.getElementById("products")
+var search = document.getElementById("search")
+var productlist = collectionsconatiner.querySelectorAll("div")
+search.addEventListener("keyup",function(){
+    var enteredvalue = event.target.value.toUpperCase()
+    for(count=0;count<productlist.length;count+=1)
+    {
+        var productname = productlist[count].querySelector("p").textContent
+        if(productname.toUpperCase().indexOf(enteredvalue)<0)
+        {
+            productlist[count].style.display="none"
+        }
+        else
+        {
+            productlist[count].style.display="block"
+        }
+
+    }
+})
